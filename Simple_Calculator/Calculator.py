7205f006aeecd96732ea780182317cc41fe572dd
sharoncy <30626642+sharoncy@users.noreply.github.com>
2022-09-28 19:40:33 +0200

    CalculatorGUI

---
+++ Simple_Calculator/Calculator.py
@@ -0,0 +1,58 @@
+'''
+num1 = input("Enter a number.")
+num2 = input("Enter another number.")
+Operator = input("Would you like to +, -, * or /?")
+
+def define_operator():
+    if Operator == "+":
+        return Operator
+    elif Operator == "-":
+        return Operator
+    elif Operator == "*":
+        return Operator
+    elif Operator == "/":
+        return Operator
+    else:
+        print("Type a valid operator: +, -, * or /.")
+
+def main():
+    print(int(num1),define_operator,int(num2)
+
+main()
+        
+
+'''
+
+print("Select an operation to perform: ")
+print("1. ADD")
+print("2. SUBSTRACT")
+print("3. MULTIPLY")
+print("4. DIVIDE")
+
+the_operation = input()
+
+if int(the_operation) == 1:
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number:")
+    print(int(num1)+int(num2))
+elif int(the_operation) == 2:
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number:")
+    print(int(num1)-int(num2))
+elif int(the_operation) == 3:
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number:")
+    print(int(num1)*int(num2))
+elif int(the_operation)2 == 4:
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number:")
+    print(int(num1)/int(num2))
+else:
+    print("Invalid Entry")
+
+
+#Simple GUI Calculator in Python, NeuralNine
+
+
+
+    
