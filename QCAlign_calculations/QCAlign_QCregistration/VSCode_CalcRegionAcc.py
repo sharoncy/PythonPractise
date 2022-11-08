82e73a4caf04d4b88aa57e19da491497b064f7dc
sharoncy <30626642+sharoncy@users.noreply.github.com>
2022-11-08 15:06:36 +0100

    Convert .iloc to .loc to make code more robust
    Advice given by Harry

--- QCAlign_calculations/QCAlign_QCregistration/VSCode_CalcRegionAcc.py
+++ QCAlign_calculations/QCAlign_QCregistration/VSCode_CalcRegionAcc.py
@@ -10,6 +10,7 @@ def CalcRegionAcc(infile, outfile):
     region_name = list(df.iloc[::5,1])
     region_accuracy = list(df.iloc[1::5,3])
     region_inaccuracy = list(df.iloc[2::5,3])
+    region_uncertainty= list(df.iloc[3::5,3])
     combined_list = list(zip(region_id,region_name,region_accuracy,region_inaccuracy))
     new_combined_list = [(a, b, c, d) for a, b, c, d in combined_list if c != 0]
 
@@ -22,8 +23,8 @@ def CalcRegionAcc(infile, outfile):
         new_region_name.append(b)
         accuracy_score.append(c / (c + d))
 
-    dict = {'Region ID': new_region_id, 'Region': new_region_name, 'Perc accuracy': accuracy_score}  
+    dict = {'Region ID': new_region_id, 'Region': new_region_name, 'Perc accuracy': accuracy_score}
     df = pd.DataFrame(dict) 
-    df.to_csv(outfile, index=False)
+    df.to_csv(outfile, index=False, sep = ";")
 
 CalcRegionAcc("AJ_QControl_Thionine_stats_SY.txt", "AJ_SY_accuracy_score_final.csv")
